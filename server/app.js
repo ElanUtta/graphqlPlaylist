beb5c1aeb54c2b24f97570a31eb69c335cfdc987
Elan Utta <elanlima19@hotmail.com>
2020-04-21 20:22:04 -0300

    Conectando ao MongoDB Atlas

--- server/app.js
+++ server/app.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+
+
+const uri = "mongodb+srv://elan:12345@cluster0-k6auz.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    if (err) {
+        console.log("Error on connection", err)
+    } else {
+        console.log("Connected successfully to server");
+
+        const db = client.db("Playlist")
+        console.log("Connected successfully to DB");
+
+        module.exports.db = db
+    }
+});
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -13,3 +32,4 @@ app.use('/graphql', graphqlHTTP({
 app.listen(4000, () => {
     console.log("Listening for requests on port 4000")
 })
+
