843899d04211c82e61006b40dae7fdb449734c6c
Elan Utta <elanlima19@hotmail.com>
2020-05-01 22:54:54 -0300

    Inserindo CRUD utilizando mongocliente

---
+++ server/models/author.js
@@ -0,0 +1,32 @@
+const client = require('../app')
+const assert = require('assert');
+
+
+const insertDoc = async (data) => {
+
+    try {
+        return client.db.collection("author").insertOne(data)
+    } catch (err) {
+        console.log(`Error ao criar Book1 ${err}`)
+    }
+}
+
+const FindById = async (id) => {
+    try {
+        return client.db.collection('author').findOne({ _id: id })
+    } catch (err) {
+        console.log(`Error ao procurar Author: ${err}`)
+    }
+}
+
+const FindAll = async (id) => {
+    try {
+        return client.db.collection('author').find({})
+    } catch (err) {
+        console.log(`Error ao procurar todos os Author: ${err}`)
+    }
+}
+
+module.exports.insertAuthor = insertDoc;
+module.exports.FindById = FindById;
+module.exports.FindAll = FindAll;
--- server/models/book.js
+++ server/models/book.js
@@ -2,15 +2,46 @@ const client = require('../app')
 const assert = require('assert');
 
 
-const insertDoc = (data) => {
-    client.db.collection("book").insertOne(data, (err, r) => {
-        assert.equal(null, err);
-        assert.equal(1, r.insertedCount);
+const insertDoc = async (data) => {
 
-    })
-
-
-
+    try {
+        return client.db.collection("book").insertOne(data)
+    } catch (err) {
+        console.log(`Error ao criar Book ${err}`)
+    }
 }
 
+const FindById = async (id) => {
+    try {
+        return client.db.collection('book').findOne({ _id: id })
+    } catch (err) {
+        console.log(`Error ao procurar book: ${err}`)
+    }
+}
+
+const FindAll = async () => {
+    try {
+        return client.db.collection('book').find({})
+    } catch (err) {
+        console.log(`Error ao procurar todos os Books: ${err}`)
+    }
+}
+
+/* 
+const insertDoc = async (data) => {
+    let a = await client.db.collection("book").insertOne(data)
+    console.log(a)
+}*/
+
+
 module.exports.insertBook = insertDoc;
+module.exports.FindById = FindById;
+module.exports.FindAll = FindAll;
+
+/*
+    Promisse é para decidir o que fazer depois que a funcao
+    termina de ser executada, contudo se voce quer pegar um valor
+    que retorne dela, voce deve utilizar async/await pois são eles
+    que irão esperar a funcao terminar de executar para poder retornar
+    o valor.
+*/
